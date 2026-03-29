2587227f4c2ffcdec7e73289aecc0d69d5db6089
mgul1 <mgul18291@gmail.com>
2026-03-28 22:35:04 -0400

    implemented the study view

---
+++ src/Deck.tsx
@@ -0,0 +1,43 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
+import { faChevronRight } from "@fortawesome/free-solid-svg-icons/faChevronRight";
+import { useRef } from "react";
+
+// deck component part of study component
+function Deck() {
+    const name = "SENG 645 Exam 1";
+    const number = 1;
+    const total = 5;
+    const front = "Front of card " + number;
+    const back = "Back of card " + number;
+    const cardRef = useRef<HTMLDivElement>(null);
+    const leftRef = useRef<HTMLDivElement>(null);
+    const rightRef = useRef<HTMLDivElement>(null);
+
+    function flipCard() {
+        if (cardRef.current) {
+            cardRef.current.classList.toggle("flip");
+        }
+    }
+
+    return (
+        <div className="main-content">
+            <div className="title">{name}</div>
+            <div className="study-deck">
+                <div onClick={() => flipCard()} ref={cardRef} id="study-card">
+                    <div id="study-card-inner">
+                        <div id="study-card-front">{front}</div>
+                        <div id="study-card-back">{back}</div>
+                    </div>
+                </div>
+                <div id="deck-nav">
+                    <span ref={leftRef}><FontAwesomeIcon icon={faChevronLeft} /></span>
+                    <span>{number}/{total}</span>
+                    <span ref={rightRef}><FontAwesomeIcon icon={faChevronRight} /></span>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Deck;
---
+++ src/Study.tsx
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar"
+import Deck from "./Deck"
+
+// study component of application
+function Study() {
+    return (
+    <>
+      <Navbar />
+      <Deck />
+    </>
+  );
+}
+
+export default Study;
