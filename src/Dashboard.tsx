bca599ebb49912c2abfd4bb25a4824e53e6b4ad1
ChelseaOkoroji <159299248+ChelseaOkoroji@users.noreply.github.com>
2026-03-29 11:31:16 -0400

    Committing to my branch

---
+++ src/Dashboard.tsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import "./Dashboard.css";
+
+const Dashboard = () => {
+
+    const [deckClicked, setdeckClicked] = useState<string | null>(null);
+
+    return (
+        <div className='main'>
+             <span className="title">Flashier Cards</span>
+            <nav className='toolbar'>
+                {( !deckClicked &&
+                    <>
+                    <button>Create</button>
+                    </>
+                )}
+                {( deckClicked &&
+                <>
+                    <button>Study</button>
+                    <button>Edit</button>
+                    <button>Rename</button>
+                    <button>Delete</button>
+                    <button onClick={() => setdeckClicked(null)}>Cancel</button>
+                </>
+                )}
+            </nav>
+            <form className='decks-list'>
+                <input type="radio" 
+                    id="deck1"
+                    name="deck" 
+                    checked={deckClicked=== "deck1"}
+                    onChange={() => setdeckClicked("deck1")}/>
+                <label htmlFor='deck1'>SENG 645 Exam 1</label>
+                <input type="radio" id="deck2" name="deck" checked={deckClicked === "deck2"} onChange={() => setdeckClicked("deck2")}/>
+                <label htmlFor='deck2'>SENG 645 Exam 1</label>
+                <input type="radio" id="deck3" name="deck" checked={deckClicked=== "deck3"}
+                onChange={() => setdeckClicked("deck3")}/>
+                <label htmlFor='deck3'>SENG 645 Exam 1</label>
+            </form>
+        </div>
+    );
+
+};
+export default Dashboard;
