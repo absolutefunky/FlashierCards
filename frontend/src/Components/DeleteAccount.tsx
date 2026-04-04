0e42df19460abb32fb68c28b777ad732cd0eb654
mgul1 <mgul18291@gmail.com>
2026-04-03 21:33:02 -0400

    completed delete account implementation

--- frontend/src/Components/DeleteAccount.tsx
+++ frontend/src/Components/DeleteAccount.tsx
@@ -1,4 +1,6 @@
 import { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
 
 function DeleteAccount() {
 	const [showOverlay, setShowOverlay] = useState(false);
@@ -10,19 +12,20 @@ function DeleteAccount() {
     return (
 		<>
 			<div id="profile-content">
-				<div>Delete Account</div>
-				<div>If you no longer wish to use Flashier Cards, you can permanently delete your account.</div>
-				<button onClick={() => showProfileOverlay(true)}>Delete My Account</button>	
+				<div className="signup-subtitle">Delete Account</div>
+				<div className="profile-text">If you no longer wish to use Flashier Cards, you can permanently delete your account.</div>
+				<button id="blue-btn" onClick={() => showProfileOverlay(true)}>Delete My Account</button>	
 			</div>
 			<div style={{display: showOverlay ? "flex" : "none"}}  className="overlay">
-				<button onClick={() => showProfileOverlay(false)}>X</button>
-				<div>Are you sure you want to delete your account? This action cannot be undone.</div>
+				<div className="cancel-action"><FontAwesomeIcon icon={faCircleXmark} onClick={() => showProfileOverlay(false)} /></div>
+				<div className="signup-subtitle" style={{fontWeight: 600}}>Delete My Account</div>
+				<div className="profile-text">Are you sure you want to delete your account? This action cannot be undone.</div>
 				<form action="">
 					<div>
-						<input id="password" type="password" />
-						<label htmlFor="password">Please enter your password to confirm.</label>
+						<div className="signup-subtitle">Please enter your password to confirm.</div>
+						<input type="password" />
 					</div>
-					<button>Delete</button>
+					<button id="blue-btn">Delete</button>
 				</form>
 			</div>
 		</>
