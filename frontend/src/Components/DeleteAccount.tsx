d445e52ec2352e856d3ff0d8ffda31a68d35cc0b
mgul1 <mgul18291@gmail.com>
2026-04-03 20:08:38 -0400

    set up overlay in profile

--- frontend/src/Components/DeleteAccount.tsx
+++ frontend/src/Components/DeleteAccount.tsx
@@ -1,6 +1,24 @@
+import { useState } from 'react';
+
 function DeleteAccount() {
+	const [showOverlay, setShowOverlay] = useState(false);
+
+	function showProfileOverlay(request: boolean) {
+        setShowOverlay(request);
+    }
+	
     return (
-        <div id="profile-content">delete account</div>
+		<>
+			<div id="profile-content">
+				<div>Delete Account</div>
+				<div>If you no longer wish to use Flashier Cards, you can permanently delete your account.</div>
+				<button onClick={() => showProfileOverlay(true)}>Delete My Account</button>	
+			</div>
+			<div style={{display: showOverlay ? "flex" : "none"}}  className="overlay">
+				<button onClick={() => showProfileOverlay(false)}>X</button>
+				<div>overlay content</div>
+			</div>
+		</>
     );
 }
 
