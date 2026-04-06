807fef4dbce23ec00172bf1593581f8055c02da8
absolutefunky <aafilak1@umbc.edu>
2026-04-06 15:40:22 -0400

    working on the exact style for all the new pages

--- frontend/src/Components/ForgotPassword.tsx
+++ frontend/src/Components/ForgotPassword.tsx
@@ -2,14 +2,16 @@ import {Link} from "react-router-dom";
 
 function ForgotPassword() {
     return (
-        <div className="main">
-            <span className="title">Flashier Cards</span>
+        <div className="verify-form">
+            <span className="verify-title">Flashier Cards</span>
             <form action="">
                 <div>
-                    <div className="sub-title">Email</div>
-                    <input type="email" required />
+                    <div className="verify-sub-title">Email</div>
+                    <input type="text" required />
                 </div>
-                <Link className="log-in-btn" to="/verify">Continue</Link>
+                <Link className="verify-form-blue-btn" to="/verify">
+                    Continue
+                    </Link>
             </form>
         </div>
     );
