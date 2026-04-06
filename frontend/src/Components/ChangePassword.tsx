632f9edf4930146ec26d3486826815fd2ee571d2
ChelseaOkoroji <159299248+ChelseaOkoroji@users.noreply.github.com>
2026-04-06 15:23:34 -0400

    Entering final css changes in changepassword

--- frontend/src/Components/ChangePassword.tsx
+++ frontend/src/Components/ChangePassword.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faX } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function ChangePassword() {
 const [passwordOverlay, setPasswordOverlay] = useState(false);
@@ -6,20 +9,36 @@ function showPasswordOverlay(request:boolean){
     setPasswordOverlay(request);
 }
     return (
-        <div id="profile-content">
-            <input type="password" placeholder="Enter Current Password"/>
-            <input type="password" placeholder="Enter New Password" />
-            <input type="password" placeholder="Confirm New Password" />
-            <button onClick={()=> showPasswordOverlay(true)}>Confirm Password Change</button>
+        <div id="password-content">
+        <span>Enter the information below to confirm password change!</span>
+            <div className="signup-subtitle">Current Password</div>
+            <input className= "password-confirm" type="password"/>
+            <div className="signup-subtitle">New Password</div>
+            <input className="password-confirm" type="password" />
+            <div className="signup-subtitle">New Password</div>
+            <input className="password-confirm" type="password" />
+            <button className="input-button" onClick={()=> showPasswordOverlay(true)}>
+                <span className="shadow-input"></span>
+                <span className="edge-input"></span>
+                <span className="front-input">CREATE</span>
+            </button>
             {
                 passwordOverlay && (
-                    <div className="overlay">
+                    <div className="password-overlay">
                       <div className="signup-subtitle"> Are you completely sure you'd like to change your password?</div>
-                        <button onClick={()=>showPasswordOverlay(false)}>
-                                YES
+                        <button className="input-button" onClick={()=>showPasswordOverlay(false)}>
+                               <span className="shadow-input"></span>
+                                <span className="edge-input"></span>
+                                <span className="front-input">
+                                    <FontAwesomeIcon icon={faCheck} />
+                                </span>
                         </button>
-                        <button onClick={()=>showPasswordOverlay(false)}>
-                            NO
+                        <button className="input-button" onClick={()=>showPasswordOverlay(false)}>
+                            <span className="shadow-input"></span>
+                            <span className="edge-input"></span>
+                            <span className="front-input">
+                                <FontAwesomeIcon icon={faX} />
+                            </span>
                         </button>
                        
                     </div>
