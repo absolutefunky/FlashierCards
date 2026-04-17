ffd8b1ec5c0164e43ae2b6ca18ac9a724ecad330
Maheen <mgul18291@gmail.com>
2026-04-17 15:46:05 -0400

    updated profile endpoints

--- backend/FlashierCards.Api/Dtos/CreateDtos/CreateProfileDto.cs
+++ backend/FlashierCards.Api/Dtos/CreateDtos/CreateProfileDto.cs
@@ -3,6 +3,5 @@ namespace FlashierCards.Api.Dtos.CreateDtos;
 // use this record when creating a new user profile
 
 public record CreateProfileDto (
-    int UserId,
-    string AnimationType
+    int UserId
 );
--- backend/FlashierCards.Api/Endpoints/ProfileEndpoints.cs
+++ backend/FlashierCards.Api/Endpoints/ProfileEndpoints.cs
@@ -1,4 +1,3 @@
-using FlashierCards.Api.Dtos;
 using FlashierCards.Api.Dtos.CreateDtos;
 using FlashierCards.Api.Dtos.ReturnDtos;
 using FlashierCards.Api.Dtos.UpdateDtos;
@@ -10,10 +9,10 @@ public static class ProfileEndpoints
 {
     public static void MapProfileEndpoints(this WebApplication app)
     {
-        // GET /users/id/profiles
+        // GET /users/{id}/profiles
         app.MapGet("/users/{id}/profiles", async(int id, Supabase.Client supabase) =>
         {
-            // find user who has the given id
+            // find user profile with given id
             var response = await supabase
                 .From<Profile>()
                 .Where(p => p.UserId == id)
@@ -31,101 +30,69 @@ public static class ProfileEndpoints
             (
                 profile.Id,
                 profile.UserId,
-                profile.AnimationType
+                profile.AnimationType!
             );
 
             return Results.Ok(profileDto);
         });
 
-        // POST /users/id/profiles
+        // POST /users/{id}/profiles
         app.MapPost("/users/{id}/profiles", async(int id, CreateProfileDto request, Supabase.Client supabase) =>
-        {
-            // find user who has the given id
-            var response = await supabase
-                .From<Profile>()
-                .Where(p => p.UserId == id)
-                .Get();
-
-            var user = response.Models.FirstOrDefault();
-
-            // create profile if user doesn't have one
-            if (user is null)
-            {
-                var profile = new Profile
-                {
-                    UserId = request.UserId,
-                    AnimationType = request.AnimationType
-                };
-
-                // insert profile into database
-                var newResponse = await supabase
-                    .From<Profile>()
-                    .Insert(profile);
-
-                profile = newResponse.Models.FirstOrDefault();
-
-                if (profile is null)
-                {
-                    return Results.NotFound();
-                }
-
-                // create a profile record to return
-                var profileDto = new ReturnProfileDto
-                (
-                    profile.Id,
-                    profile.UserId,
-                    profile.AnimationType
-                );
-
-                return Results.Ok(profileDto);            
-            }
-            return Results.NotFound();
-        });
-
-        // PUT /users/id/profiles
-        app.MapPut("/users/{id}/profiles", async(int id, UpdateProfileDto request, Supabase.Client supabase) =>
         {
             var profile = new Profile
             {
-                AnimationType = request.AnimationType
+                UserId = request.UserId
             };
 
-           // update password hash in user table
-            var response = await supabase
+            // insert profile into database
+            var newResponse = await supabase
                 .From<Profile>()
-                .Where(p => p.UserId == id)
-                .Set(p => p.BackgroundColor!, profile.BackgroundColor)
-                .Set(p => p.AnimationType!, profile.AnimationType)
-                .Update();
+                .Insert(profile);
 
-            profile = response.Models.FirstOrDefault();
+            profile = newResponse.Models.FirstOrDefault();
 
             if (profile is null)
             {
                 return Results.NotFound();
             }
 
-            // create a user record to return
+            // create a profile record to return
             var profileDto = new ReturnProfileDto
             (
                 profile.Id,
                 profile.UserId,
-                profile.AnimationType
+                profile.AnimationType!
             );
 
             return Results.Ok(profileDto);
         });
 
+        // PUT /users/{id}/profiles
+        app.MapPut("/users/{id}/profiles", async(int id, UpdateProfileDto request, Supabase.Client supabase) =>
+        {
+            // update animation type in profile table
+            var response = await supabase
+                .From<Profile>()
+                .Where(p => p.UserId == id)
+                .Set(p => p.AnimationType!, request.AnimationType)
+                .Update();
 
-        // GET /users/{id}/profile 
-        // use ReturnProfileDto record to return data
+            var profile = response.Models.FirstOrDefault();
 
-        // POST /users/{id}/profile to create user profile
-        // use CreateProfileDto record for request data
-        // use ReturnProfileDto record to return data
+            if (profile is null)
+            {
+                return Results.NotFound();
+            }
 
-        // PUT /user{id}/profile/updateTheme
-        // use UpdateProfileDto record for request data
-        // use ReturnProfileDto record to return data
+            // create a profile record to return
+            var profileDto = new ReturnProfileDto
+            (
+                profile.Id,
+                profile.UserId,
+                profile.AnimationType!
+            );
+
+            return Results.Ok(profileDto);
+        });
     }
 }
