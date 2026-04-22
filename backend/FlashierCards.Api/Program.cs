1a9032aaedce40e3af60947791dc995130eb76ab
Maheen <mgul18291@gmail.com>
2026-04-21 21:02:11 -0400

    modified program

--- backend/FlashierCards.Api/Program.cs
+++ backend/FlashierCards.Api/Program.cs
@@ -3,8 +3,11 @@ using FlashierCards.Api.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 // database url and public key from Supabase
-var url = builder.Configuration.GetConnectionString("SUPABASE_URL");
-var key = builder.Configuration.GetConnectionString("SUPABASE_KEY");
+//var url = builder.Configuration.GetConnectionString("SUPABASE_URL");
+//var key = builder.Configuration.GetConnectionString("SUPABASE_KEY");
+
+var url = builder.Configuration.GetSection("SUPABASE_URL").Get<string>();
+var key = builder.Configuration.GetSection("SUPABASE_KEY").Get<string>();
 
 var options = new Supabase.SupabaseOptions
 {
@@ -19,7 +22,7 @@ await supabase.InitializeAsync();
 builder.Services.AddSingleton(supabase);
 
 // configure CORS to establish connection with frontend
-var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string[]>();
+var allowedOrigins = builder.Configuration.GetSection("AllowedOrigins").Get<string>();
 
 builder.Services.AddCors(options =>
 {
